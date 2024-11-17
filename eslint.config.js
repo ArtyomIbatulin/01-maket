cf473253456b192dfa345b557e3d206db219ea7d
ArtyomIbatulin <64503017+ArtyomIbatulin@users.noreply.github.com>
2024-11-17 19:04:25 +0300

    feat: eslint dont works

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,56 @@
+import js from '@eslint/js';
 import globals from 'globals';
-import pluginJs from '@eslint/js';
+import eslintReact from 'eslint-plugin-react';
+import eslintReactHooks from 'eslint-plugin-react-hooks';
+import eslintReactRefresh from 'eslint-plugin-react-refresh';
+import prettierPlugin from 'eslint-plugin-prettier';
+import eslintConfigPrettier from 'eslint-config-prettier';
 import tseslint from 'typescript-eslint';
-import pluginReact from 'eslint-plugin-react';
-import prettierConfig from 'eslint-config-prettier';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [
+export default tseslint.config(
   {
-    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
-    languageOptions: { globals: globals.browser },
-  },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-  prettierConfig,
-
-  {
-    rules: {
-      'prettier/prettier': 'error',
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      react: eslintReact,
+      'react-hooks': eslintReactHooks,
+      'react-refresh': eslintReactRefresh,
+      prettier: prettierPlugin,
     },
   },
-];
+  {
+    ignores: ['node_modules', 'dist', 'eslint.config.js'],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.browser,
+        ...globals.es2020,
+      },
+      parserOptions: {
+        project: ['tsconfig.json', 'tsconfig.app.json', 'tsconfig.node.json'],
+      },
+    },
+  },
+  {
+    files: ['**/*.{ts, tsx}'],
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+      ...eslintConfigPrettier.rules,
+      'react-refresh/only-export-components': [
+        'warn',
+        { allowConstantExport: true },
+      ],
+      'prefer-const': 'error',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      'no-unused-expressions': [
+        'error',
+        { allowShortCircuit: true, allowTernary: true },
+      ],
+    },
+  }
+);
